b8a4bb937e0afd5f2e758ec4cb21f08fed4e7f1f
amitnyc83 <amit_nyc@live.com>
2019-08-03 14:04:27 -0400

    add cart table with associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   validates_uniqueness_of :username
   has_secure_password
   has_secure_token :auth_token
+  has_many :carts
 
 
   def invalidate_token
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_02_153134) do
+ActiveRecord::Schema.define(version: 2019_08_03_175441) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.string "name"
+    t.integer "quantity"
+    t.decimal "total_price"
+    t.boolean "ordered"
+    t.bigint "user_id"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_carts_on_product_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
