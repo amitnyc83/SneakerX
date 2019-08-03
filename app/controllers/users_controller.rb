8914f58fb31dfc8f0caec419903cd0a9f9f436ab
amitnyc83 <amit_nyc@live.com>
2019-08-03 13:44:30 -0400

    able to authenticate user with token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,25 @@
 class UsersController < ApplicationController
 
+  def create
+    user = User.create!(user_params)
+    # exclamtion point so it gives us the user back that was just created
+     # and an error message if there is one
+    render json: { token: user.auth_token }
+  end
+
+  def profile
+    user = User.find_by_auth_token!(request.headers[:token])
+    render json: { user: { username: user.username, email: user.email, name: user.name } }
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email, :name)
+  end 
+
+
+
+
 end
