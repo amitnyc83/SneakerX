8531f90115e0d99ab2646c14850f2869b4209bf3
amitnyc83 <amit_nyc@live.com>
2019-08-04 16:44:20 -0400

    create products table && add address columns to users

---
+++ db/migrate/20190804204033_add_user_type_and_address_info_to_users.rb
@@ -0,0 +1,10 @@
+class AddUserTypeAndAddressInfoToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :type, :string
+    add_column :users, :address, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :country, :string
+    add_column :users, :zipcode, :string
+  end
+end
